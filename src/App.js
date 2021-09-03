94f0fc229135286002f3c30c4d3657f75de1e4e8
Alemarnery <aleemar.95@gmail.com>
2021-09-02 22:21:37 -0400

    helpers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
 import Listado from "./components/Listado";
@@ -10,11 +10,24 @@ const App = () => {
   const [restante, setRestante] = useState(0);
   const [mostrarpregunta, setMostrarpregunta] = useState(true);
   const [gastos, guardarGastos] = useState([]);
+  const [gasto, guardarGasto] = useState({});
+  const [creargasto, guardarCrearGasto] = useState(false);
 
-  //Cuando agreguemos un nuevo gasto
-  const agregarNuevoGasto = (gasto) => {
-    guardarGastos([...gastos, gasto]);
-  };
+  //Actualiza el restante
+
+  useEffect(() => {
+    if (creargasto) {
+      //agrega el nuevo presupuesto
+      guardarGastos([...gastos, gasto]);
+    }
+
+    //resta del presupuesto actual
+    const presupuestoRestante = restante - gasto.cantidad;
+    setRestante(presupuestoRestante);
+
+    //Resetear el form
+    guardarCrearGasto(false);
+  }, [gasto]);
 
   return (
     <div className="container">
@@ -30,7 +43,10 @@ const App = () => {
           ) : (
             <div className="row">
               <div className="one-half column">
-                <Formulario agregarNuevoGasto={agregarNuevoGasto} />
+                <Formulario
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
               </div>
 
               <div className="one-half column">
